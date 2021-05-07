68e355fe5ff34dd374a39f8e7d9777022c764647
RitZ <71270153+Ritika-dey@users.noreply.github.com>
2021-05-07 19:00:47 +0530

    Create MaxHeap-implementation.cpp

---
+++ MaxHeap-implementation.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int size = 9, curr =0;
+int arr[9]; 
+void insert(int val){ 
+    if(curr<size){
+        arr[curr] = val;
+        int i= curr;
+        while(i > 0 and arr[(i-1)/2] < arr[i]){ 
+             int temp = arr[(i-1)/2];
+             arr[(i-1)/2] = arr[i];
+             arr[i] = temp;
+             i = (i-1)/2; 
+        }
+        curr++;
+    }
+    else{
+        cout<<"Heap is full"<<endl;
+    }
+}
+int main() {
+    int val;
+    cin>>val;
+    insert(val);
+    while(1){
+        cin>>val;
+        if(val == -1) break;
+        insert(val);
+    }
+
+    cout<<"Given heap is : ";
+    for(int i=0;i<curr;i++){
+        cout<<arr[i]<<" ";
+    }
+}
